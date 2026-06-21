18f8fda70287b9cf103c25efc310f2df1ca3b966
hazmat1986 <cgsodak@gmail.com>
2026-06-20 21:15:36 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,11 @@
+name = "Oswin Voss"
+rank = "Junior Mage"
+health = 30
+energy = 100
+attack = 22
+defence = 5
+evasion = 10
+max_health = 30
+
+print(f"Brehon Rider Field Record\n{'-'*60}\nName:\t\t{name}\nRank:\t\t{rank}\nHealth:\t\t{health}\nEnergy:\t\t{energy}")
+print(f"Attack:\t\t{attack}\nDefence:\t{defence}\nEvasion:\t{evasion}\nMax Health:\t{max_health}")
