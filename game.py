c15d6ef63cf82de41e9e7c0620a3d74af57e219c
hazmat1986 <cgsodak@gmail.com>
2026-06-20 22:18:02 -0500

    Update game.py
    Updated to Chapter 3

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ attack = 22
 defence = 5
 evasion = 10
 max_health = 30
+spell_list = ["Fireball","Magic Missile","Meditate"]
 
 print(f"Brehon Rider Field Record\n{'-'*60}\nName:\t\t{name}\nRank:\t\t{rank}\nHealth:\t\t{health}\nEnergy:\t\t{energy}")
 print(f"Attack:\t\t{attack}\nDefence:\t{defence}\nEvasion:\t{evasion}\nMax Health:\t{max_health}")
+print(f"Spell List:\t{spell_list[0]} \n\t\t{spell_list[1]} \n\t\t{spell_list[2]}") 
